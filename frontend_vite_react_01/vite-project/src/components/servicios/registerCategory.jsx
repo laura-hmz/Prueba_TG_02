c4dfd9c875552049f24b699365ac72de13ad437d
Lau <52580838+laura-hmz@users.noreply.github.com>
2023-10-16 02:32:06 -0500

    visual para ofrecer servicios

--- frontend_vite_react_01/vite-project/src/components/servicios/registerCategory.jsx
+++ frontend_vite_react_01/vite-project/src/components/servicios/registerCategory.jsx
@@ -1,31 +1,29 @@
 import { Link } from 'react-router-dom';
+import { FaCar, FaGraduationCap, FaBed, FaEllipsisH } from 'react-icons/fa';
+
+const buttonStyle = "md:w-60 md:h-60 w-40 h-40 rounded-lg p-4 m-4 bg-white shadow-2xl text-center flex flex-col items-center justify-center transition-transform transform hover:scale-105 ";
 
 const RegisterCategory = () => {
-  const buttonStyle = {
-    outline: 'none',
-    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-    borderRadius: '8px',
-    padding: '12px',
-    fontWeight: 'bold',
-    cursor: 'pointer',
-  };
-
   return (
-    <div className="text-center mt-40">
-      <h1 className='block mb-5'>Hola, ¿qué deseas publicar?</h1>
+    <div className="text-center md:mt-20 md:mb-24 mt-16 mb-20">
+      <h1 className='block md:mb-14 mb-16 md:text-3xl text-2xl text-gray-600 font-semibold'>¡Hola! ¿Qué deseas ofrecer?</h1>
 
-      <div className="flex justify-center space-x-4">
-        <Link to="/register/transport" style={{ ...buttonStyle, backgroundColor: '#FFD700' }}>
-          Registrar Servicio de Transporte
+      <div className="flex flex-wrap justify-center">
+        <Link to="/register/transport" className={buttonStyle}>
+          <FaCar size={80} className="text-yellow-500" />
+          Servicio de Transporte
         </Link>
-        <Link to="/register/academic-advising" style={{ ...buttonStyle, backgroundColor: '#007BFF' }}>
-          Registrar Asesorías Académicas
+        <Link to="/register/academic-advising" className={buttonStyle}>
+          <FaGraduationCap size={80} className="text-blue-500" />
+          Asesorías Académicas
         </Link>
-        <Link to="/register/room" style={{ ...buttonStyle, backgroundColor: '#800080' }}>
-          Registrar Servicio de Habitaciones
+        <Link to="/register/room" className={buttonStyle}>
+          <FaBed size={80} className="text-purple-500" />
+          Servicio de Habitaciones
         </Link>
-        <Link to="/register/other" style={{ ...buttonStyle, backgroundColor: '#FFA500' }}>
-          Registrar Otros Servicios
+        <Link to="/register/other" className={buttonStyle}>
+          <FaEllipsisH size={80} className="text-orange-500" />
+          Otros Servicios
         </Link>
       </div>
     </div>
