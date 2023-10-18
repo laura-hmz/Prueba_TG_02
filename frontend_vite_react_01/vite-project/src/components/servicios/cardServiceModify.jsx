28a66c6ebb96a3f6089b0f4e0e0587e68e551812
Lau <52580838+laura-hmz@users.noreply.github.com>
2023-10-18 01:46:58 -0500

    imagenes en Cards

--- frontend_vite_react_01/vite-project/src/components/servicios/cardServiceModify.jsx
+++ frontend_vite_react_01/vite-project/src/components/servicios/cardServiceModify.jsx
@@ -39,18 +39,18 @@ const CardServiceModify = ({services, onDeleteService}) => {
                 : service.tipo_servicio === 'Servicio de habitaciones' ?
                     <img
                         className="lg:h-48 md:h-36 w-full object-cover object-center"
-                        src={"https://img.freepik.com/free-psd/3d-rendering-parts-house_23-2150123666.jpg?w=740&t=st=1696591308~exp=1696591908~hmac=86327bd17f8cef0b2be7996d4fb4d6dd2fbde2c1dafc993d72074995d90239e8"}
+                        src={"https://img.freepik.com/foto-gratis/vista-modelo-casa-3d_23-2150761062.jpg?w=740&t=st=1697606643~exp=1697607243~hmac=e15ed78a73552e93e0b617b94ec61d78dbfa1357d45d0204a826906c8a32c5e5"}
                         alt={service._id}
                     /> 
                 : service.tipo_servicio === 'Asesorías Académicas' ?
                     <img
                             className="lg:h-48 md:h-36 w-full object-cover object-center"
-                            src={"https://img.freepik.com/free-psd/education-concept-with-supplies_23-2149935204.jpg?w=740&t=st=1696591646~exp=1696592246~hmac=832ff8bef9e43f403c303430331656b1c54a6fb168a1a2ca3d7ff4b3b76f466c"}
+                            src={"https://img.freepik.com/psd-gratis/representacion-3d-naturaleza-muerta-telefono_23-2150425344.jpg?w=740&t=st=1697607616~exp=1697608216~hmac=b48370e6d111dc052189f1cb74fafc8027a209efdfa7ae6a96a51b4e0e320f6f"}
                             alt={service._id}
                         /> 
                 :   <img
                         className="lg:h-48 md:h-36 w-full object-cover object-center"
-                        src={"https://img.freepik.com/free-psd/3d-rendering-shopping-concept_23-2149877674.jpg?w=740&t=st=1696592116~exp=1696592716~hmac=ba9f556c80553885a386028e154119086218114aa44b97c21ec2f2aa664aaa4c"}
+                        src={"https://img.freepik.com/foto-gratis/ilustracion-3d-telefono-inteligente-scooter-entrega-cajas-bolsas-papel_58466-14576.jpg?w=740&t=st=1697607462~exp=1697608062~hmac=f9c4be1e62389c35e5e0b16092d968d1641840572e03bec795123aff3b3109c9"}
                         alt={service._id}
                     />  
                 }
