b9d5500cb4a15590488e0f385e7de72e793b4656
Lau <52580838+laura-hmz@users.noreply.github.com>
2023-10-25 20:23:37 -0500

    inicio Cambios

--- frontend_vite_react_01/vite-project/src/components/login/home2.jsx
+++ frontend_vite_react_01/vite-project/src/components/login/home2.jsx
@@ -4,28 +4,37 @@ const Home2 = () => {
 
   
     return (
-        <div className="container my-12 mx-auto md:px-6">
+        
+        <section className="antialiased min-h-screen font-sans bg-[#1c1b1b]">
+            <div className="container mx-auto">
+                <div className="flex flex-wrap  relative">
+                    <div className="   lg:w-9/10  md:w-9/10 w-full mx-auto relative">
+                        <div className="bg-white  mt-4   shadow-xl border border-gray-100 rounded-lg px-8 py-4 overflow-hidden relative">
+                            <div className="grid mx-auto  flex items-center lg:grid-cols-2 lg:gap-x-12 md:px-10 md:py-8 lg:px-10 lg:py-8  ">
+                                <div className="mb-8 lg:mb-0 md:mb-0  md:ml-4 text-center md:text-left">
+                                <h2 className="   text-5xl font-bold  leading-tight tracking-tight">
+                                    Publica y Adquiere  
+                                </h2>
+                                <h3 className="  mb-10 text-gray-900 text-5xl font-bold leading-tight tracking-tight">
+                                Servicios
+                                </h3>
+                                <LoginButton/>
+                                </div>
 
-            <section className="mb-32 text-center lg:text-left">
-                <div className="px-6 py-12 md:px-12">
-                    <div className="grid items-center lg:grid-cols-2 lg:gap-x-12">
-                        <div className="mb-12 lg:mb-0">
-                            <h2 className="my-12 text-5xl font-bold leading-tight tracking-tight">
-                                Publica y Adquiere  <br />
-                                <span className="text-success dark:text-success-400">Servicios</span>
-                            </h2>
-                            <LoginButton/>
-                        </div>
+                                <div className=" mt-4 md:mt-0 lg:mt-0 rounded-lg border-solid border-2 border-gray-300 shadow-2xl">
+                                    <img src="https://res.cloudinary.com/dt0ejpyba/image/upload/v1698281702/copia_mpuiek.jpg"
+                                        className="w-full  md:h-[500px] rounded-lg object-cover" alt="" />
+                                </div>
+                            </div>
+                    
 
-                        <div className="mb-12 lg:mb-0">
-                            <img src="https://i.ibb.co/9bd4P8c/banner-mix-2.jpg"
-                                className="w-full rounded-lg shadow-lg dark:shadow-black/20" alt="" />
                         </div>
                     </div>
                 </div>
-            </section>
-
-        </div>
+            </div>
+        
+        </section>
+       
     );
 };
 
--- frontend_vite_react_01/vite-project/src/components/login/loginButton.jsx
+++ frontend_vite_react_01/vite-project/src/components/login/loginButton.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button className="group relative h-12 w-48 overflow-hidden rounded-2xl bg-green-500 text-lg font-bold text-white" onClick={() => loginWithRedirect()}>Acceder</button>;
+  return <button className="group border-solid border-2 border-gray-200 relative shadow-2xl h-12 w-48 overflow-hidden rounded-2xl bg-[#80CC29] text-lg font-bold text-white hover:text-green-100 hover:border-white hover:border-solid hover:border-[1px] hover:bg-green-500" onClick={() => loginWithRedirect()}>Acceder</button>;
 };
 
 export default LoginButton;
