53f197d35dfa7960cf2e54aefb6e3b952224620c
Lau <52580838+laura-hmz@users.noreply.github.com>
2023-10-31 06:49:58 -0500

    acomodo no found con info

--- frontend_vite_react_01/vite-project/src/components/loaders/notFoundLoaders/folderLoader.jsx
+++ frontend_vite_react_01/vite-project/src/components/loaders/notFoundLoaders/folderLoader.jsx
@@ -12,8 +12,12 @@ const FolderLoader = () => {
             <div className={divGridSub}>
                 <h1 className="mt-4 lg:mt-28 md:ml-4 text-center text-2xl lg:text-4xl font-serif text-gray-500">
                     No has registrado ningún servicio  </h1>
-                    <h1 className="  mt-2 text-center text-xl lg:text-3xl font-serif text-gray-400">
+                    <h1 className="  mt-2 text-center text-xl lg:text-3xl font-serif text-gray-400 mb-6">
                     ¡No esperes más para ofrecer!</h1>
+                    <p className="mt-2 text-center text-sm md:text-base lg:text-md text-gray-500">
+                        Dirigete a *Ofrecer servicios* en la barra de navegación </p>
+                    <p className="mt-2 text-center text-sm md:text-base lg:text-md text-gray-500">
+                        y registra los servicios que deseas ofrecer</p>
                     
             </div>
 
--- frontend_vite_react_01/vite-project/src/components/loaders/notFoundLoaders/spaceLoader.jsx
+++ frontend_vite_react_01/vite-project/src/components/loaders/notFoundLoaders/spaceLoader.jsx
@@ -8,10 +8,14 @@ const SpaceLoader = () => {
     return (
         <div className={divGrid}>
             <div className={divGridSub}>
-                <h1 className="mt-4 lg:mt-28 md:ml-4 text-center text-2xl lg:text-4xl font-serif text-gray-500">
+                <h1 className="mt-4 lg:mt-28 md:ml-4 text-center text-2xl lg:text-4xl font-serif text-gray-500 ">
                     No has guardado ningún servicio  </h1>
-                    <h1 className="  mt-2 text-center text-xl lg:text-3xl font-serif text-gray-400">
+                <h1 className="  mt-2 text-center text-xl lg:text-3xl font-serif text-gray-400 mb-6">
                     ¡No esperes más para explorar!</h1>
+                <p className="mt-2 text-center text-sm md:text-base lg:text-md text-gray-500">
+                    Dirigete a *Buscar servicios* o a *Home* en la barra de navegación </p>
+                <p className="mt-2 text-center text-sm md:text-base lg:text-md text-gray-500">
+                    y guarda los servicios que deses consultar después</p>
                     
             </div>
 
