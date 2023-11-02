98e0acc9fb9c22d11e6e530a72409918a19d4ba7
Lau <52580838+laura-hmz@users.noreply.github.com>
2023-11-02 07:32:59 -0500

    Update matchmaking_processor_02.js
    rute

--- backend_node_01/src/processors/matchmaking_processor_02.js
+++ backend_node_01/src/processors/matchmaking_processor_02.js
@@ -3,7 +3,7 @@ const { PythonShell } = require('python-shell');
 // Función para ejecutar el algoritmo Python
 function procesadorPrueba(arrayOfNumbers, arrayOfIds, querySample) {
     // Ruta al script de Python
-    const pythonScriptPath = '../../python-scripts/matchmaking_algorithm.py';
+    const pythonScriptPath = '/opt/render/project/src/backend_node_01/src/python-scripts/matchmaking_algorithm.py';
 
     // Opciones para configurar la ejecución
     const options = {
