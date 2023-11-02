d7c4a891f4fa4954e1fea354988eead4afb8e5c0
Lau <52580838+laura-hmz@users.noreply.github.com>
2023-11-02 02:25:26 -0500

    prueba config

--- frontend_vite_react_01/vite-project/vite.config.js
+++ frontend_vite_react_01/vite-project/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+server: {
+host: true,
+strictPort: true,
+port: 8000,
+},
+resolve: {}
   
 })
 
